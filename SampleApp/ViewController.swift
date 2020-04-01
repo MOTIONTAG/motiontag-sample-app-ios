bc9dc3d11762e1ecd2b57d528a0325a22bf00641
Henrique Faria <henriquenunesfaria@gmail.com>
2020-04-01 10:21:19 +0200

    Add ToggleTracking button

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var toggleTrackingButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         view.backgroundColor = .white
+        addToggleTrackingButton()
+        setToggleTrackingButtonTarget()
+        setToggleTrackingButtonConstraints()
+    }
+
+    func addToggleTrackingButton() {
+        toggleTrackingButton = UIButton(type: .system)
+        toggleTrackingButton.setTitle("Start Tracking", for: .normal)
+        toggleTrackingButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(toggleTrackingButton)
+    }
+
+    func setToggleTrackingButtonTarget() {
+        toggleTrackingButton.addTarget(self,
+                                       action: #selector(handleToggleTrackingButtonTouchUpInside),
+                                       for: .touchUpInside)
+    }
+
+    func setToggleTrackingButtonConstraints() {
+        NSLayoutConstraint.activate([
+            toggleTrackingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            toggleTrackingButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+
+    @objc func handleToggleTrackingButtonTouchUpInside() {
+        print("ToggleTracking button has been tapped")
     }
 }
