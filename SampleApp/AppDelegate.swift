2314e66077c93f949ca0f4869179a17dbe11f938
Henrique Faria <henriquenunesfaria@gmail.com>
2020-04-01 10:01:53 +0200

    Initial Xcode project

---
+++ SampleApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SampleApp
+//
+//  Created by Henrique Faria on 01.04.20.
+//  Copyright © 2020 Henrique Faria. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Henrique Faria on 01.04.20.
+//  Copyright © 2020 Henrique Faria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        view.backgroundColor = .white
+    }
+}
