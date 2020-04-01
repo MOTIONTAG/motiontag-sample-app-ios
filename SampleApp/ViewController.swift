5ed7c84aa1891fe5fa8a57f7983ada9653ab6bad
Henrique Faria <henriquenunesfaria@gmail.com>
2020-04-01 17:09:27 +0200

    Add tracking switch

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class ViewController: IndicatorUIViewController {
-    var appDelegate: AppDelegate?
-    let trackingButton: UIButton = UIButton(type: .system)
+    private var appDelegate: AppDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         appDelegate = UIApplication.shared.delegate as? AppDelegate
-        ovserveSetupCompleted()
+        showProgressIndicator()
+        observeSetupCompleted()
     }
 
-    private func ovserveSetupCompleted() {
+    private func observeSetupCompleted() {
         if let appDelegate = appDelegate {
             if appDelegate.isSetupComplete {
                 setupCompleted()
             } else {
-                showProgressIndicator()
                 NotificationCenter.default.addObserver(self, selector: #selector(setupCompleted),
                                                        name: Notification.Name(appDelegate.setupCompleteNotificationName), object: nil)
             }
@@ -32,42 +31,32 @@ class ViewController: IndicatorUIViewController {
     }
 
     @objc private func setupCompleted() {
-          hideProgressIndicator()
-          addTrackingButton()
-      }
+        hideProgressIndicator()
+        addTrackingSwtich()
+    }
 
-    private func addTrackingButton() {
-        let title = getTrackingButtonTitle()
-        trackingButton.translatesAutoresizingMaskIntoConstraints = false
-        trackingButton.setTitle(title, for: .normal)
-        trackingButton.addTarget(self,
-                                 action: #selector(handleTrackingButtonTouchUpInside),
-                                 for: .touchUpInside)
-        view.addSubview(trackingButton)
+    private func addTrackingSwtich() {
+        let trackingSwitch = UISwitch()
+        trackingSwitch.isEnabled = true
+        trackingSwitch.isOn = appDelegate?.motionTag?.isTrackingActive ?? false
+        trackingSwitch.translatesAutoresizingMaskIntoConstraints = false
+
+        trackingSwitch.addTarget(self, action: #selector(switchValueDidChange(sender:)), for: .valueChanged)
+        view.addSubview(trackingSwitch)
+
         NSLayoutConstraint.activate([
-            trackingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            trackingButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            trackingSwitch.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            trackingSwitch.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
     }
 
-    private func getTrackingButtonTitle() -> String {
-        if let motionTag = appDelegate?.motionTag {
-            return motionTag.isTrackingActive ? "Stop Tracking" : "Start Tracking"
-        } else {
-            return "MotionTag SDK not initialized"
-        }
-    }
-
-    @objc private func handleTrackingButtonTouchUpInside() {
+    @objc private func switchValueDidChange(sender: UISwitch!) {
         if let appDelegate = appDelegate, let motionTag = appDelegate.motionTag {
-            if motionTag.isTrackingActive {
-                motionTag.stop()
-
-            } else {
+            if sender.isOn {
                 motionTag.start(withToken: appDelegate.jwtToken)
+            } else {
+                motionTag.stop()
             }
-            let title = getTrackingButtonTitle()
-            trackingButton.setTitle(title, for: .normal)
         }
     }
 }
